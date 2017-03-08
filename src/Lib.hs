01050d42fdd6b67da7cecb726b25603ebce76dcb
Sandeep.C.R <sandeepcr2@gmail.com>
2017-03-08 17:52:31 +0000

    Cleanup

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,7 +3,6 @@
 module Lib where
 
 import qualified Text.HTML.TagSoup as TGS
-import Lucid
 import Data.Maybe
 import Data.Text (Text, strip, pack, unpack, intercalate, concat)
 import Cases
@@ -15,46 +14,45 @@ data Status = Open | Closed deriving (Show)
 data Tag = Tag TGSTag [Tag] Status deriving (Show)
 
 getDescription :: TGSTag -> Status -> String
-getDescription tgst@(TGS.TagOpen n attr) st = n
-getDescription tgst@(TGS.TagText n) st = "text#" ++ n
-getDescription tgst@(TGS.TagComment n) st = "Comment tag"
-getDescription (TGS.TagWarning _) st = error "Unsupported tag type in document"
-getDescription (TGS.TagPosition _ _) st = error "Unsupported tag type in document"
+getDescription (TGS.TagOpen n _) _ = n
+getDescription (TGS.TagText n) _ = "text#" ++ n
+getDescription (TGS.TagComment _) _ = "Comment tag"
+getDescription (TGS.TagWarning _) _ = error "Unsupported tag type in document"
+getDescription (TGS.TagPosition _ _) _ = error "Unsupported tag type in document"
 
 prettyPrint :: Tag -> IO ()
-prettyPrint tag = prettyPrint' 0 tag
+prettyPrint = prettyPrint' 0 
   where
     prettyPrint' :: Int -> Tag -> IO ()
     prettyPrint' offset (Tag tgst cs status) = do
-      putStrLn $ (take offset $ repeat ' ') ++ (getDescription tgst status)
+      putStrLn $ indent offset ++ getDescription tgst status
       mapM_ (prettyPrint' (offset + 2)) cs 
-
-convert :: String -> IO ()
-convert html = return ()
 
 makeLucidFunction :: Tag -> String
 makeLucidFunction (Tag (TGS.TagOpen n []) [] _) = n ++ "_ []"
 makeLucidFunction (Tag (TGS.TagOpen n []) _  _) = n ++ "_ "
-makeLucidFunction (Tag (TGS.TagOpen n xs) _ _) =  n ++ "_" ++ " [" ++  (unpack $ intercalate "," $ makeAttibute <$> xs) ++ "]"
+makeLucidFunction (Tag (TGS.TagOpen n xs) _ _) =  n ++ "_" ++ " [" ++  unpack (intercalate "," $ makeAttibute <$> xs) ++ "]"
   where
     makeAttibute :: (String, String) -> Text
-    makeAttibute (a, v) = Data.Text.concat [(camelize $ pack a), "_ " , (pack $ show v)]
+    makeAttibute (a, v) = Data.Text.concat [camelize $ pack a, "_ " , pack $ show v]
+
+indent :: Int -> String
+indent offset = replicate offset ' '
 
 convertToLucid :: Tag -> String
-convertToLucid tag = convertToLucidWithOffset 0 tag
+convertToLucid = convertToLucidWithOffset 0
   where
   convertToLucidWithOffset :: Int -> Tag -> String
-  convertToLucidWithOffset offset tag@(Tag (TGS.TagText content) [] _) = (take offset $ repeat ' ') ++ show content ++ "\n"
+  convertToLucidWithOffset offset (Tag (TGS.TagText content) [] _) = indent offset ++ show content ++ "\n"
   convertToLucidWithOffset offset tag@(Tag (TGS.TagOpen _ _) [] _) =
-    (take offset $ repeat ' ') ++ (makeLucidFunction tag)  ++ "\n"
-  convertToLucidWithOffset offset tag@((Tag (TGS.TagOpen name attr) ((Tag (TGS.TagText content) [] _):[]) _)) =
-    (take offset $ repeat ' ') ++ (makeLucidFunction tag) ++ " " ++ show content ++ "\n"
-  convertToLucidWithOffset offset tag@((Tag (TGS.TagOpen _ _) children _)) =
-    (take offset $ repeat ' ') ++ (makeLucidFunction tag) ++ " " ++ (generateChildCode (offset+2) children) 
+    indent offset ++ makeLucidFunction tag  ++ "\n"
+  convertToLucidWithOffset offset tag@(Tag (TGS.TagOpen _ _) [Tag (TGS.TagText content) [] _] _) =
+    indent offset ++ makeLucidFunction tag ++ " " ++ show content ++ "\n"
+  convertToLucidWithOffset offset tag@(Tag (TGS.TagOpen _ _) children _) =
+    indent offset ++ makeLucidFunction tag ++ " " ++ generateChildCode (offset+2) children 
   generateChildCode :: Int -> [Tag] -> String
-  generateChildCode offset [] = ""
-  generateChildCode offset children = " $ do \n" ++ (Prelude.concat $ convertToLucidWithOffset offset <$> children)
-
+  generateChildCode _ [] = ""
+  generateChildCode offset children = " $ do \n" ++ Prelude.concat (convertToLucidWithOffset offset <$> children)
 
 makeDocFromHtml :: String -> Tag
 makeDocFromHtml html = buildHtmlTree $ TGS.parseTags html
@@ -63,32 +61,32 @@ buildHtmlTree :: [TGSTag] -> Tag
 buildHtmlTree (x:xs) = reverseChildren $ addTags (makeTag x) $ filterNewlines xs
   where
     filterNewlines :: [TGSTag] -> [TGSTag]
-    filterNewlines tx = filter (not.isNewLine) tx
+    filterNewlines = filter (not.isNewLine) 
       where
       isNewLine :: TGSTag -> Bool
-      isNewLine (TGS.TagText t) = (strip(pack t) == "")
+      isNewLine (TGS.TagText t) = strip(pack t) == ""
       isNewLine _ = False
     reverseChildren :: Tag -> Tag
-    reverseChildren (Tag t children st) = Tag t (reverseChildren <$> (reverse children)) st
+    reverseChildren (Tag t children st) = Tag t (reverseChildren <$> reverse children) st
     makeTag :: TGSTag -> Tag
-    makeTag tgst@(TGS.TagOpen n attr) = Tag tgst [] Open
-    makeTag tgst@(TGS.TagText n) = Tag tgst [] Closed
-    makeTag tgst@(TGS.TagComment n) = Tag tgst [] Closed
+    makeTag tgst@(TGS.TagOpen _ _) = Tag tgst [] Open
+    makeTag tgst@(TGS.TagText _) = Tag tgst [] Closed
+    makeTag tgst@(TGS.TagComment _) = Tag tgst [] Closed
     makeTag (TGS.TagWarning _) = error "Unsupported tag type in document"
     makeTag (TGS.TagPosition _ _) = error "Unsupported tag type in document"
     addTags :: Tag -> [TGSTag] -> Tag
     addTags tag [] = tag
-    addTags t (x:xs) = addTags (fromJust $ addTag t x) xs
+    addTags t (y:ys) = addTags (fromJust $ addTag t y) ys
     addTag :: Tag -> TGSTag -> Maybe Tag
     addTag (Tag _ _ Closed) _ = Nothing 
-    addTag tg@(Tag td ts Open) (TGS.TagClose _) = closeTag tg
+    addTag tg@(Tag _ _ Open) (TGS.TagClose _) = closeTag tg
     addTag (Tag td [] Open) tgst = Just $ Tag td [ makeTag tgst ] Open
     addTag (Tag td ex@(t:ts) Open) tgst = case addTag t tgst of
       Just nt -> Just $ Tag td (nt:ts) Open
-      Nothing -> Just $ Tag td ((makeTag tgst):ex) Open
+      Nothing -> Just $ Tag td (makeTag tgst:ex) Open
     closeTag :: Tag -> Maybe Tag
     closeTag (Tag _ _ Closed) = Nothing
     closeTag (Tag td [] Open) = Just (Tag td [] Closed)
     closeTag (Tag td ex@(t:ts) Open) = case closeTag t of
       Nothing -> Just $ Tag td ex Closed
-      Just x -> Just $ Tag td (x:ts) Open
+      Just y -> Just $ Tag td (y:ts) Open
